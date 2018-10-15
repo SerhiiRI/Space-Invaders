6ba68003cf8c6c2e57599fe6ca91a911f871e299
serhii <serhii@localhost.localdomain>
2018-10-16 01:03:42 +0200

    Changing structure of file

--- game_ANSI.hs
+++ game_ANSI.hs
@@ -1,12 +1,13 @@
 -- importing library to working with ANSI console
 import System.Console.ANSI as ANSI
 import System.Console.Terminal.Size
+import System.IO
 
 
 -- userShip, provides as a test, in real situation:
 -- TODO: creating User Ships must be procedural in the separate module
 userShip :: [String]
-userShip = [ "   /#\\   ", "\\ / | \\ /", "/\\\\ | //\\" ,"   - -   " ]
+userShip = [ "    /#\\    ", " \\ / | \\ / ", " /\\\\ | //\\ " ,"    - -    " ]
 
 
 -- parsing window for geting windows width char size
@@ -20,50 +21,65 @@ parseWindow (Nothing) = 0
 
 -- You may add X argumnet and line to rendering
 -- TODO: create spcefic data type to each of types
-makingBashPointRow :: Int -> String -> IO ()
-makingBashPointRow x_position line = do
-  ANSI.setCursorPosition 40 x_position
-  putStrLn line
+makingBashPointRow :: Int -> Int -> [String] -> IO ()
+makingBashPointRow y_position x_position (x:xs) = do
+  ANSI.setCursorPosition y_position x_position
+  putStrLn x
+  makingBashPointRow (y_position+1) x_position xs
+makingBashPointRow y_position x_position [] = do
+  ANSI.setCursorPosition y_position x_position
 
 
 
---printOneLine :: Int -> String -> String
---printOneLine = do
---  setCursorPosition
 
-
--- Infinite loop for main function
--- Function get kode symbol XB86_* and
--- render `userShip` in spectial parsing point
-mainLoopIO :: IO Int
-mainLoopIO = do
-  widthOfWindows <- size                                -- geting windows size
-  pointOnWindows <- fmap read getLine                   -- geting left right code
-  let bliat = returnInWidth (parseWindow widthOfWindows) pointOnWindows
-  --mapM_ putStrLn userShip
-  mapM_ (makingBashPointRow bliat) userShip
-  mainLoopIO
+getValue :: Int -> Char -> Maybe Int
+getValue value 'a' = Just (value - 1)
+getValue value 'd' = Just (value + 2)
+getValue value _ = Nothing
 
 
 
-returnInWidth :: Int -> Int -> Int
-returnInWidth width getPositionX
-        | getPositionX > width          = width - 10
-        | getPositionX < 1              = 1
+
+
+ifReadyDo :: Handle -> IO a -> IO (Maybe a)
+ifReadyDo hnd x = hReady hnd >>= f
+   where f True = x >>= return . Just
+         f _    = return Nothing
+
+
+
+
+
+returnInWidth :: Int -> Maybe Int -> Int
+returnInWidth width Nothing = width
+returnInWidth width (Just getPositionX)
+        | getPositionX > width          = width - 11
+        | getPositionX < 0              = 1
         | otherwise                     = getPositionX
 
 
 
 
-{--
-drawing :: IO a
-drawing = do
-  xPosition <- if getCursorPosition >>= 
 
-  drawing
+-- Infinite loop for main function
+-- Function get kode symbol XB86_* and
+-- render `userShip` in spectial parsing point
+mainLoopIO :: Int -> IO Int
+mainLoopIO xv = do
+  widthOfWindows <- parseWindow <$> size                        -- widthOfWindows = 69 :: IO
+  pointOnWindows <- fmap (getValue xv) getChar                  -- 
+  let xPoint = returnInWidth widthOfWindows pointOnWindows
+  --mapM_ putStrLn userShip
+  --mapM_ (makingBashPointRow xPoint) userShip
+  makingBashPointRow 50 xPoint userShip
+  mainLoopIO xv
 
-main = drawing
---}
+
+main = do
+  putStr ANSI.hideCursorCode
+  let xCoordinate = 30
+  mainLoopIO 30
+  putStr ANSI.showCursorCode
 
 
 
