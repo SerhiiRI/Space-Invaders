3d6bb185339a22e454172e18734f9d577fc2739e
serhii <serhii@localhost.localdomain>
2018-10-15 17:24:43 +0200

    Committer: serhii <serhii@localhost.localdomain>
     On branch mainrender
     Changes to be committed:
    	modified:   game_ANSI.hs

--- game_ANSI.hs
+++ game_ANSI.hs
@@ -1,5 +1,5 @@
 -- importing library to working with ANSI console
-import System.Console.ANSI
+import System.Console.ANSI as ANSI
 import System.Console.Terminal.Size
 
 
@@ -9,10 +9,21 @@ userShip :: [String]
 userShip = [ "   /#\\   ", "\\ / | \\ /", "/\\\\ | //\\" ,"   - -   " ]
 
 
-
+-- parsing window for geting windows width char size
+-- TODO: change geting tuple for geting demention
+-- using for parsing "size" command library:
+---- System.Console.Terminal.Size
 parseWindow :: (Integral n) => Maybe (Window n) -> n
 parseWindow (Just (Window {height = h, width = w})) = w
 parseWindow (Nothing) = 0
+
+
+-- You may add X argumnet and line to rendering
+-- TODO: create spcefic data type to each of types
+makingBashPointRow ::  Int -> String -> IO ()
+makingBashPointRow x_position line = do
+  ANSI.setCursorPosition 40 x_position
+  putStrLn line
 
 
 
@@ -21,13 +32,17 @@ parseWindow (Nothing) = 0
 --  setCursorPosition
 
 
-chujIO :: IO Int
-chujIO = do
-  suka <- size
-  xpoint <- fmap read getLine
-  let bliat = returnInWidth (parseWindow suka) xpoint
-  print bliat
-  chujIO
+-- Infinite loop for main function
+-- Function get kode symbol XB86_* and
+-- render `userShip` in spectial parsing point
+mainLoopIO :: IO Int
+mainLoopIO = do
+  widthOfWindows <- size                                -- geting windows size
+  pointOnWindows <- fmap read getLine                   -- geting left right code 
+  let bliat = returnInWidth (parseWindow widthOfWindows) pointOnWindows
+  --mapM_ putStrLn userShip
+  --makingBashPointRow bliat userShips
+  mainLoopIO
 
 
 
